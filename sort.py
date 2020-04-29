a209585d7015da613dcb5a9a6b5afb07fc7d5bed
Suny <55714870+Sunney-X@users.noreply.github.com>
2020-04-29 05:16:18 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -4,24 +4,22 @@ from time import sleep
 
 def sort():
     try:
-        json_data = load(open('bannable.json', 'r'))
-    except (FileNotFoundError, JSONDecodeError):
-        print("Error! File does not exist or is poorly formated")
-        sleep(2)
+        json_data = load(open('data.json', 'r'))
 
-    try:
         f = open('bannedusers.txt', 'r')
+        bannedusers = f.readlines()
+        bannedusers = [banneduser.replace('\n', '') for banneduser in bannedusers]
+   
+        bannable = []
+        
+        for user in json_data:
+            if user['reportable'] and user['name'] not in bannedusers:
+                bannable.append(user)
+
+        dump(bannable, open('bannable.json', 'w'), indent=4)
+        f.close()
+
+        return bannable
     except FileNotFoundError:
         print('\'bannedusers.txt\' Not found')
-    bannedusers = f.readlines()
-    bannedusers = [banneduser.replace('\n', '') for banneduser in bannedusers]
 
-    bannable = []
-
-    for user in json_data:
-        if user['reportable'] and user['name'] not in bannedusers:
-            bannable.append(user)
-
-    dump(bannable, open('bannable.json', 'w'), indent=4)
-
-    return bannable
